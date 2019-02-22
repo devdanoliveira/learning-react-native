b1015231d7635f712159074e8f815de277825583
Daniel Oliveira <dev.danoliveira@gmail.com>
2019-02-22 14:27:20 -0300

    first commit

---
+++ app/sections/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Text from  'react-native'
+
+class Header extends React.Component {
+    render() {
+        return (<Text></Text>);
+    }
+}
---
+++ app/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class Home extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello React Native</Text>
+            </View>
+        );
+    }
+}
